9693c0103bdff8edbc913f121ffb3d40ff6115f8
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-03-02 18:43:12 +0300

    created a dashboard layout

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,3 +1,22 @@
-export default function DashboardLayout({ children }) {
-  return <div>{children}</div>;
+import Sidebar from "@/components/Sidebar";
+import "../../globals.css";
+
+export const metadata = {
+  title: "Tech Blogger",
+  description: "Get informed about the latest tech news",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="">
+        <main className="mx-auto  grid grid-cols-12">
+          <div className="h-screen col-span-2 pl-10 pt-10 border-gray-200 border-r ">
+            <Sidebar />
+          </div>
+          <div className="col-span-10  pt-10 pl-10">{children}</div>
+        </main>
+      </body>
+    </html>
+  );
 }
